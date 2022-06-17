2c980ecb9b53e2a67c3aa5da08cda86d2613d1c7
ChrisSpescha <SpeschaChris@gmail.com>
2022-06-17 12:52:31 -0400

    Register Function Implemented

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask, jsonify, request
+from flask import Flask, render_template, redirect, url_for, flash, abort, request, jsonify
+from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user, login_required
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
+from forms import RegisterForm, LoginForm
 from datetime import datetime
 from flask_marshmallow import Marshmallow
 
@@ -14,74 +17,112 @@ db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
 
-class Articles(db.Model):
+# CONFIGURE Database
+class User(UserMixin, db.Model):
+    __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100))
-    body = db.Column(db.Text())
-    date = db.Column(db.DateTime, default=datetime.now)
-
-    def __init__(self, title, body):
-        self.title = title
-        self.body = body
-
-# db.create_all()
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
 
 
-class ArticleSchema(ma.Schema):
+db.create_all()
+
+
+# class ArticleSchema(ma.Schema):
+#     class Meta:
+#         fields = ('id', 'email', 'password', 'name')
+
+# article_schema = ArticleSchema()
+# articles_schema = ArticleSchema(many=True)
+
+
+class UserSchema(ma.Schema):
     class Meta:
-        fields = ('id', 'title', 'body', 'date')
+        fields = ('id', 'email', 'password', 'name')
 
 
-article_schema = ArticleSchema()
-articles_schema = ArticleSchema(many=True)
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
 
 
-@app.route('/', methods=['GET'])
-def get_articles():
-    all_articles = Articles.query.all()
-    results = articles_schema.dump(all_articles)
-    return jsonify(results)
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    name = request.json['name']
+    email = request.json['email']
+    password = request.json['password']
 
+    print("got here first")
 
-@app.route('/get/<id>/', methods=['GET'])
-def post_details(id):
-    article = Articles.query.get(id)
-    return article_schema.jsonify(article)
+    print(name)
+    print(email)
+    print(password)
+    new_user = User(
+                email=email,
+                password=password,
+                name=name
+                )
+    print("got here second")
 
-
-@app.route('/add', methods=['GET', 'POST'])
-def add_articles():
-    title = request.json['title']
-    body = request.json['body']
-    print(title)
-    print(body)
-    articles = Articles(title, body)
-    db.session.add(articles)
+    db.session.add(new_user)
     db.session.commit()
-    return article_schema.jsonify(articles)
+    print(jsonify(new_user))
+    return users_schema.jsonify(new_user)
+
+    # form = RegisterForm()
+    # if form.validate_on_submit():
+    #
+    #     if User.query.filter_by(email=form.email.data).first():
+    #         print(User.query.filter_by(email=form.email.data).first())
+    #         # User already exists
+    #         flash("You've already signed up with that email, log in instead!")
+    #         return redirect(url_for('login'))
+    #     # Hashing and Salting Password
+    #     hash_and_salted_password = generate_password_hash(
+    #         form.password.data,
+    #         method='pbkdf2:sha256',
+    #         salt_length=8
+    #     )
+    #     new_user = User(
+    #         email=form.email.data,
+    #         name=form.name.data,
+    #         password=hash_and_salted_password,
+    #     )
 
 
-@app.route('/update/<id>/', methods=['PUT'])
-def update_article(id):
-    article = Articles.query.get(id)
-
-    title = request.json['title']
-    body = request.json['body']
-
-    article.title = title
-    article.body = body
-    db.session.commit()
-    return article_schema.jsonify(article)
-
-
-@app.route('/delete/<id>/', methods=['DELETE'])
-def article_delete(id):
-    print(request.method)
-    article = Articles.query.get(id)
-    db.session.delete(article)
-    db.session.commit()
-
-    return article_schema.jsonify(article)
+# @app.route('/add', methods=['GET', 'POST'])
+# def add_articles():
+#     title = request.json['title']
+#     body = request.json['body']
+#     print(title)
+#     print(body)
+#     articles = Articles(title, body)
+#     db.session.add(articles)
+#     db.session.commit()
+#     return article_schema.jsonify(articles)
+#
+#
+# @app.route('/update/<id>/', methods=['PUT'])
+# def update_article(id):
+#     article = Articles.query.get(id)
+#
+#     title = request.json['title']
+#     body = request.json['body']
+#
+#     article.title = title
+#     article.body = body
+#     db.session.commit()
+#     return article_schema.jsonify(article)
+#
+#
+# @app.route('/delete/<id>/', methods=['DELETE'])
+# def article_delete(id):
+#     print(request.method)
+#     article = Articles.query.get(id)
+#     db.session.delete(article)
+#     db.session.commit()
+#
+#     return article_schema.jsonify(article)
 
 
 if __name__ == "__main__":
--- src + ui/app.js
+++ src + ui/app.js
@@ -1,50 +1,27 @@
+const links = document.querySelectorAll('link[rel="import"]');
+links.forEach((link) => {
+    let template = link.import.querySelector('template');
+    let clone = document.importNode(template.content, true);
+    let target = clone.querySelector(".template").dataset.tab;
+    document.getElementById(target).appendChild(clone);
+});
+
+window.$ = window.jquery = require("jquery");
+window.popper = require("popper.js");
+require("bootstrap");
+require('./renderer.js')
+
+
 const myform = document.getElementById('myform')
-const title = document.getElementById('title')
-const body = document.getElementById('description')
+const name = document.getElementById('name')
+const password = document.getElementById('password')
+const email = document.getElementById('email')
+
 
 const articles = document.getElementById('articles')
 
-let articleId;
+let userId;
 
-//const insertData = (newData) => {
-//    fetch('http://127.0.0.1:5000/add', {
-//        method:'POST',
-//        headers: {
-//            'Content-Type': 'application/json'
-//        },
-//        body:JSON.stringify(newData)
-//        })
-//        .then(resp => resp.json())
-//        .then((data) => {
-//            console.log(data)
-//        })
-//        .catch(error => console.log(error))
-//    }
-
-const getAllData = () => {
-
-    fetch('http://127.0.0.1:5000/', {
-        method:'GET',
-        headers: {
-            'Content-Type': 'application/json'
-            }
-        })
-        .then(resp => resp.json())
-        .then(data => renderArticles(data))
-        .catch(error => console.log(error))
-
-    }
-
-//const deleteData = (id) => {
-//    fetch(`http://127.0.0.1:5000/delete/${id}/` , {
-//        method:'DELETE',
-//         headers:{
-//            'Content-Type': 'application/json'
-//        }
-//    })
-//    getAllData()
-//}
-//
 //const getDataByID = (id) => {
 //    fetch(`http://127.0.0.1:5000/get/${id}/`, {
 //        method:'GET',
@@ -57,14 +34,96 @@ const getAllData = () => {
 //        renderOneItem(data)
 //    })
 //}
+
+const renderOneItem = (mydata) => {
+    name.value = mydata.name
+    email.value = mydata.email
+    password.value = mydata.password
+
+    userId = mydata.id
+}
+
+const insertData = (newData) => {
+    fetch('http://127.0.0.1:5000/register', {
+        method:'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body:JSON.stringify(newData)
+        })
+        .then(console.log(newData))
+        .then(console.log(resp))
+        .then(resp => resp.json())
+        .then((data) => {
+            console.log(data)
+        })
+        .catch(error => console.log(error))
+    }
+
+myform.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const newData = {
+        email:email.value,
+        password:password.value,
+        name:name.value,
+    }
+    if(userId) {
+        updateData(userId, newData)
+    } else {
+        insertData(newData)
+    }
+    myform.reset()
+})
+
+//getAllData()
+
+//function renderArticles(mydata){
+//    articles.innerHTML = '';
+//    mydata.forEach(data => {
+//        articles.innerHTML += `
+//        <div class = "card card-body my-2">
+//            <p>${data.title}</p>
+//            <p>${data.body}</p>
+//            <h5>${data.date}</h5>
 //
-//const renderOneItem = (mydata) => {
-//    title.value = mydata.title
-//    body.value = mydata.body
-//
-//    articleId = mydata.id
+//            <p>
+//            <button class = "btn btn-danger" onclick = "deleteData('${data.id}')">Delete</button>
+//            <button class = "btn btn-success" onclick = "getDataByID('${data.id}')">Update</button>
+//            </p>
+//        </div>
+//        `
+//    })
 //}
+
+
+
+//const getAllData = () => {
 //
+//    fetch('http://127.0.0.1:5000/', {
+//        method:'GET',
+//        headers: {
+//            'Content-Type': 'application/json'
+//            }
+//        })
+//        .then(resp => resp.json())
+//        .then(data => renderArticles(data))
+//        .catch(error => console.log(error))
+//
+//    }
+
+//const deleteData = (id) => {
+//    fetch(`http://127.0.0.1:5000/delete/${id}/` , {
+//        method:'DELETE',
+//         headers:{
+//            'Content-Type': 'application/json'
+//        }
+//    })
+//    getAllData()
+//}
+
+
+
 //const updateData = (articleId, mydata) => {
 //    fetch(`http://127.0.0.1:5000/update/${articleId}/`, {
 //        method:'PUT',
@@ -78,41 +137,6 @@ const getAllData = () => {
 //        getAllData()
 //    })
 //}
-//
-//
-//
-function renderArticles(mydata){
-    articles.innerHTML = '';
-    mydata.forEach(data => {
-        articles.innerHTML += `
-        <div class = "card card-body my-2">
-            <p>${data.title}</p>
-            <p>${data.body}</p>
-            <h5>${data.date}</h5>
 
-            <p>
-            <button class = "btn btn-danger" onclick = "deleteData('${data.id}')">Delete</button>
-            <button class = "btn btn-success" onclick = "getDataByID('${data.id}')">Update</button>
-            </p>
-        </div>
-        `
-    })
-}
 
-myform.addEventListener('submit', (e) => {
-    e.preventDefault()
 
-    const newData = {
-        title:title.value,
-        body:body.value
-    }
-
-    if(articleId) {
-        updateData(articleId, newData)
-    } else {
-        insertData(newData)
-    }
-    myform.reset()
-})
-
-getAllData()
